82d273a50cd9c53b9ebd06f09768be2859214ac6
Nikolay Troshkov <ariser@ariser.ru>
2016-03-28 09:10:29 +0300

    sa hw1

---
+++ y3s2/SA/HW1/hw-01-java/src/lab_01/Square.java
@@ -0,0 +1,24 @@
+package lab_01;
+
+/*public abstract class Square implements Shape {
+    public abstract void setColor(String s);
+}*/
+
+/*public abstract class Square implements Shape {
+    public abstract void setColor(String s) {}
+}*/
+
+/*public abstract class Square extends Shape {
+    public void setColor(String s) {
+    }
+}*/
+
+/*public abstract class Square implements Shape {
+    public void setColor(Integer i) {}
+}*/
+
+public abstract class Square implements Shape {
+    public void setColor(String s) {}
+
+    public void setColor(Integer i) {}
+}
